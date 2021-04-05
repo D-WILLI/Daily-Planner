bc12029db01d02791a510c7065521ef50a3600a3
Diamond <williams.diamond2392@gmail.com>
2021-04-04 22:33:57 -0400

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Daily-Planner
+
+## Table of Contents
+1. [Description](#description)
+2. [How to Use ](#use)
+3. [Demo](#demo)
+
+
+## Description 
+<a name="description"></a>
+This project is a Daily Work Planner. It was created to allow a user to use a Daily Planner to schedule out their day by the hour. In this application the User will be shown:
+1. Current Week, Month, Day, Hour and Minute.
+    a. ex: Sunday - Jan.15,2021 at 9:13 AM.
+2. The agenda will have hourly time slots for 6am through 9pm. 
+3. The agenda will be color sorted by time to reflect: 
+    a. A light gray color for hours that have passed. 
+    b. A pink color for the current hour. 
+    c. A blue color for the upcoming hours.
+3. The User can type the in their agenda item in the text slot.
+4. The user will then click the add to agenda button.  
+5. The User's input will be saved in the text section on the corresponding time.  
+
+
+## How To Use  
+<a name="use"></a>
+How To add an item to the agenda. 
+1. Click on the empty box next to the time slot 
+2. Type in your agenda item for the hour.
+3. Click the blue (add agenda button) at the end of text box. 
+Your agenda item will be saved, even if you close or refresh the webpage. 
+
+How to delete an item in the agenda.
+1. Click on the text in the box next to the time slot 
+2. Type in your new agenda item to replace the current agenda item or leave it blank.
+3. Click the blue (add agenda button) at the end of text box. 
+Your new agenda item will be saved and you old items will be deleted.  
+
+
+## Demo 
+<a name="demo"></a>
+![MyGiph](https://media.giphy.com/media/zTpmTaihlshQ5jliXi/giphy.gif)
--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/*
+Pseudo Code 
+The user will be shown an agenda with the current Day and Time 
+    1. Preferably in the Format 
+        Day of the Week, Month, Day , Hour , Minute. 
+The agenda will have the times 6am - 9pm 
+The User will have the option to type in agenda text box. 
+When the user wants to save their agenda item they will click the add to calendar button.
+The button will save the users text and will display in the text box.
+    1. This will be stored in local storage 
+The calendar calendar will show the following 
+    1. A light gray color for time that has passed 
+    2. A pink color for the current hour 
+    3. A blue color for upcoming times. */
+
 const displayDate = document.querySelector ("#currentDay"); 
 let agendaItems= document.querySelector (".form-control"); 
 let textBox = document.querySelector(".col"); 
@@ -42,10 +57,10 @@ let time6 = "6 AM ";
 let time7 = "7 AM "; 
 let time8 = "8 AM "; 
 let time9 = "9 AM "; 
-let time10 = "10 AM "; 
+let time10 = "11 PM "; 
 let time11 = "11 AM "; 
 let time12 = "12 PM "; 
-let time1 = "1 PM "; 
+let time1 = "12 PM "; 
 let time2 = "2 PM "; 
 let time3 = "3 PM "; 
 let time4 = "4 PM "; 
@@ -55,14 +70,14 @@ let time07 = "7 PM ";
 let time08 = "8 PM "; 
 let time09 = "9 PM "; 
 
+// testing
 console.log(currentTime > time09); 
 console.log(currentTime === time09); 
 console.log(currentTime < time09); 
 
-console.log
 // This is where we are creating conditions for the colors on the agenda 
 
-// color changer for 6am 
+// color changer rules for 6am 
 if ( currentTime > time6){
     document.querySelector('#hour-6 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-6 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -76,7 +91,7 @@ if ( currentTime > time6){
     document.querySelector('#hour-6 .form-control').style.backgroundColor="#5bb1cd"; 
 }; 
 
-// color changer for 7am 
+// color changer rules for 7am 
 if ( currentTime > time7){
     document.querySelector('#hour-7 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-7 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -90,7 +105,7 @@ if ( currentTime > time7){
     document.querySelector('#hour-7 .form-control').style.backgroundColor="#5bb1cd"; 
 }; 
 
-// color changer for 8am 
+// color changer rules for 8am 
 if ( currentTime > time8){
     document.querySelector('#hour-8 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-8 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -104,7 +119,7 @@ if ( currentTime > time8){
     document.querySelector('#hour-8 .form-control').style.backgroundColor="#5bb1cd"; 
 }; 
 
-// color changer for 9am 
+// color changer rules for 9am 
 if ( currentTime > time9){
     document.querySelector('#hour-9 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-9 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -118,7 +133,7 @@ if ( currentTime > time9){
     document.querySelector('#hour-9 .form-control').style.backgroundColor="#5bb1cd"; 
 }; 
 
-// color changer for 10am 
+// color changer rules for 10am 
 if ( currentTime > time10){
     document.querySelector('#hour-10 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-10 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -132,7 +147,7 @@ if ( currentTime > time10){
     document.querySelector('#hour-10 .form-control').style.backgroundColor="#5bb1cd"; 
 }; 
 
-// color changer for 11am 
+// color changer rules for 11am 
 if ( currentTime > time11){
     document.querySelector('#hour-11 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-11 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -146,7 +161,7 @@ if ( currentTime > time11){
     document.querySelector('#hour-11 .form-control').style.backgroundColor="#5bb1cd"; 
 };
 
-// color changer for 12pm 
+// color changer rules for 12pm 
 if ( currentTime > time12){
     document.querySelector('#hour-12 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-12 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -160,7 +175,7 @@ if ( currentTime > time12){
     document.querySelector('#hour-12 .form-control').style.backgroundColor="#5bb1cd"; 
 };
 
-// color changer for 1pm 
+// color changer rules for 1pm 
 if ( currentTime > time1){
     document.querySelector('#hour-1 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-1 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -174,7 +189,7 @@ if ( currentTime > time1){
     document.querySelector('#hour-1 .form-control').style.backgroundColor="#5bb1cd"; 
 }; 
 
-// color changer for 2pm 
+// color changer rules for 2pm 
 if ( currentTime > time2){
     document.querySelector('#hour-2 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-2 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -188,7 +203,7 @@ if ( currentTime > time2){
     document.querySelector('#hour-2 .form-control').style.backgroundColor="#5bb1cd"; 
 };
 
-// color changer for 3pm 
+// color changer rules for 3pm 
 if ( currentTime > time3) {
     document.querySelector('#hour-3 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-3 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -202,7 +217,7 @@ if ( currentTime > time3) {
     document.querySelector('#hour-3 .form-control').style.backgroundColor="#5bb1cd"; 
 }; 
 
-// color changer for 4pm 
+// color changer rules for 4pm 
 if ( currentTime > time4){
     document.querySelector('#hour-4 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-4 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -216,7 +231,7 @@ if ( currentTime > time4){
     document.querySelector('#hour-4 .form-control').style.backgroundColor="#5bb1cd"; 
 }; 
 
-// color changer for 5pm 
+// color changer rules for 5pm 
 if ( currentTime > time5){
     document.querySelector('#hour-5 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-5 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -230,7 +245,7 @@ if ( currentTime > time5){
     document.querySelector('#hour-5 .form-control').style.backgroundColor="#5bb1cd"; 
 };
 
-// color changer for 06pm 
+// color changer rules for 06pm 
 if ( currentTime > time06){
     document.querySelector('#hour-06 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-06 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -244,7 +259,7 @@ if ( currentTime > time06){
     document.querySelector('#hour-06 .form-control').style.backgroundColor="#5bb1cd"; 
 };
 
-// color changer for 07pm 
+// color changer rules for 07pm 
 if ( currentTime > time07){
     document.querySelector('#hour-07 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-07 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -258,7 +273,7 @@ if ( currentTime > time07){
     document.querySelector('#hour-07 .form-control').style.backgroundColor="#5bb1cd"; 
 };
 
-// color changer for 08pm 
+// color changer rules for 08pm 
 if ( currentTime > time08) {
     document.querySelector('#hour-08 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-08 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -272,7 +287,7 @@ if ( currentTime > time08) {
     document.querySelector('#hour-08 .form-control').style.backgroundColor="#5bb1cd"; 
 }; 
 
-// color changer for 09pm 
+// color changer rules for 09pm 
 if (currentTime > time09){
     document.querySelector('#hour-09 .input-group-text').style.backgroundColor="#D3D3D3"; 
     document.querySelector('#hour-09 .form-control').style.backgroundColor="#D3D3D3"; 
@@ -288,21 +303,3 @@ if (currentTime > time09){
 
 
 
-
-
- 
-
-
-
-/*
-The user will be shown an agenda will the current Day and Time 
-    1. Preferably in the Format 
-        Day of the Week, Month, Day , Hour , Minute 
-** The User can click on a box next to time and will have the option to type in text 
-The user clicks the add to calendar button 
-The user can then save the information inside the text box 
-    This will be stored in local storage 
-The calendar calendar will show the following 
-    1. A color for time that has passed 
-    2. A color for current time 
-    3. A color for upcoming times. */
